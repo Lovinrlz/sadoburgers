3e4cf3cc25109f6695b0684e27c3b79f4a67ba32
Lovinrlz <david2291rlz@gmail.com>
2025-06-25 15:25:38 -0500

    Add files via upload

---
+++ js/menuburger.js
@@ -0,0 +1,13 @@
+// Funcion para abrir y cerrar menu hamburguesa (NO TOCAR)
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+  nav.classList.add("visible");
+});
+
+cerrar.addEventListener("click", () => {
+  nav.classList.remove("visible");
+});
+// Fin de funcion.
