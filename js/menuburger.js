ef46c5b2beeb3aa2c5eb309ce905a8b58ca7dcc0
Lovinrlz <david2291rlz@gmail.com>
2025-07-01 17:54:43 -0500

    Update menuburger.js

--- js/menuburger.js
+++ js/menuburger.js
@@ -2,12 +2,20 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const overlay = document.querySelector("#overlay");
 
 abrir.addEventListener("click", () => {
   nav.classList.add("visible");
+  overlay.classList.add("visible");
 });
 
 cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
+  overlay.classList.remove("visible");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("visible");
+  overlay.classList.remove("visible");
 });
 // Fin de funcion.
