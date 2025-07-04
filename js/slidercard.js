af5d8b054d61616f4c2ce84e9e6a19beff3fbc02
Lovinrlz <david2291rlz@gmail.com>
2025-07-04 14:48:56 -0500

    Update slidercard.js

--- js/slidercard.js
+++ js/slidercard.js
@@ -1,3 +1,4 @@
+// Funcion imagenes en carrusel
 document.addEventListener("DOMContentLoaded", function () {
   // Inicializar los tres carruseles
   setupCarousel("card1");
@@ -6,27 +7,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function setupCarousel(cardId) {
     const card = document.getElementById(cardId);
+    const slides = card.querySelectorAll(".image-slide");
+    let currentSlide = 0;
+
+    function nextSlide() {
+      const activeSlide = card.querySelector(".image-slide.active");
+      const nextSlide = slides[(currentSlide + 1) % slides.length];
+
+      activeSlide.classList.remove("active");
+      activeSlide.classList.add("prev");
+
+      nextSlide.classList.add("active");
+      nextSlide.classList.remove("next");
+
+      setTimeout(() => {
+        activeSlide.classList.remove("prev");
+        activeSlide.classList.add("next");
+      }, 1000);
+
+      currentSlide = (currentSlide + 1) % slides.length;
+    }
+    // Cambiar imagen cada 3 segundos
+    let slideInterval = setInterval(nextSlide, 2500);
+
     // Click a la card para su redireccionamiento
     card.addEventListener("click", function (e) {
       if (!e.target.classList.contains("btn")) {
         window.location.href = card.dataset.url;
       }
     });
-    const slides = card.querySelectorAll(".image-slide");
-    let currentSlide = 0;
-
-    function nextSlide() {
-      // Ocultar la imagen actual
-      slides[currentSlide].classList.remove("active");
-
-      // Avanzar ala siguiente imagen
-      currentSlide = (currentSlide + 1) % slides.length;
-
-      // Mostrar la nueva imagen
-      slides[currentSlide].classList.add("active");
-    }
-
-    // Cambiar imagen cada 3 segundos
-    let slideInterval = setInterval(nextSlide, 3500);
   }
 });
